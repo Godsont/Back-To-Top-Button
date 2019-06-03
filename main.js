25cf21f5c24f6e3b9af0f6b010cb98813d72130f
Godson Thomas <godson.aocmusic@gmail.com>
2019-06-03 17:22:05 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
